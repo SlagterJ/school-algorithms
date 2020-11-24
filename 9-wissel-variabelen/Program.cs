0bb5144205de5d940197563c88c84da547458d2a
SlagterJ <slagterj2001@gmail.com>
2020-11-24 18:22:34 +0100

    Added wissel variabelen

---
+++ 9-wissel-variabelen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _9_wissel_variabelen {
+  class Program {
+    static void Main(string[] args) {
+      int a = 10;
+      int b = 20;
+      int c;
+
+      Console.WriteLine($"A is {a}");
+      Console.WriteLine($"B is {b}\n");
+
+      c = a;
+      a = b;
+      b = c;
+
+      Console.WriteLine($"A is {a}");
+      Console.WriteLine($"B is {b}");
+    }
+  }
+}
