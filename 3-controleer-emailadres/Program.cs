b18e1d3d5a61e8041e87253217ae5df4e4d241c9
SlagterJ <slagterj2001@gmail.com>
2020-11-24 10:41:57 +0100

    Made checking mailaddress 50 thousand times simpler.

--- 3-controleer-emailadres/Program.cs
+++ 3-controleer-emailadres/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Mail;
 
 namespace _3_controleer_emailadres {
   class Program {
@@ -10,46 +11,19 @@ namespace _3_controleer_emailadres {
 
       // Ask the user for input of the email address
       Console.WriteLine("Uw E-Mail adres:");
-      // Convert the input string into an array of single characters
-      char[] userInput = Console.ReadLine().ToCharArray();
+      string userInput = Console.ReadLine();
 
-      // If the input is less than 5 characters, it isn't an email (a@b.c)
-      if (userInput.Length < 5) {
-        NotCorrect();
-        return;
-      }
-
-      // Check if '@' exists
-      if (!Array.Exists(userInput, element => element == '@')) {
-        NotCorrect();
-        return;
-      }
-
-      // Check if '.' exists
-      if (!Array.Exists(userInput, element => element == '.')) {
-        NotCorrect();
-        return;
-      }
-
-      // Check if the '.' that does exist has a higher index
-      // than the '@'
-      // No need to check for null values as they would've already
-      // been caught
-      int indexOfAt = Array.IndexOf(userInput, '@');
-      // Last index, seeing as you can use multiple dots in an email
-      int indexOfDot = Array.LastIndexOf(userInput, '.');
-      if (indexOfDot < indexOfAt) {
-        NotCorrect();
+      try {
+        // Try to create a new mailaddress from the string
+        new MailAddress(userInput);
+      } catch (FormatException) {
+        // If it fails write that it's incorrect
+        Console.WriteLine("Dat is NIET een correcte E-Mail.");
         return;
       }
 
       // If we've gotten to this point the email is correct
-      Console.WriteLine("Dat is een correcte E-Mail!");
-    }
-
-    // Function to echo that the email isn't correct
-    static void NotCorrect() {
-      Console.WriteLine("Dat is GEEN correcte E-Mail!");
+      Console.WriteLine("Dat is een correcte E-Mail.");
     }
   }
 }
