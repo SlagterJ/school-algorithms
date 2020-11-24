097997b43a460e6e9beaba91a6b00539f6f6af1a
SlagterJ <slagterj2001@gmail.com>
2020-11-24 09:56:26 +0100

    Added maak een string assignment

---
+++ 1-maak-een-string/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1_maak_een_string {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("---");
+      Console.WriteLine("Opdracht 1: Maak een string");
+      Console.WriteLine("211252 Jordy Slagter");
+      Console.WriteLine("---\n\n");
+
+      // Count from char Z to char A
+      for (char i = 'Z'; i >= 'A'; i--) {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
