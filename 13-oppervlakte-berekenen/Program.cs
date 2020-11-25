25f0f738c5be2d3231ad00347fa473d5afdfe3fd
SlagterJ <slagterj2001@gmail.com>
2020-11-25 13:55:33 +0100

    Added oppervlakte berekenen

---
+++ 13-oppervlakte-berekenen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _13_oppervlakte_berekenen {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("De diameter:");
+      string userInput = Console.ReadLine();
+
+      double diameter;
+      bool parseSucceed = Double.TryParse(userInput, out diameter);
+
+      if (!parseSucceed) {
+        Console.WriteLine("That isn't a number!");
+        return;
+      }
+
+      double radius = diameter / 2;
+
+      double surface = Math.PI * (radius * radius);
+      double circumference = Math.PI * diameter;
+
+      Console.WriteLine($"De oppervlakte is {surface}");
+      Console.WriteLine($"De omtrek is {circumference}");
+    }
+  }
+}
