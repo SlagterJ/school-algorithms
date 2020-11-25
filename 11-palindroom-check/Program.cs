8c6a71520dc867ca63203e31245ec4f6bc942d7f
SlagterJ <slagterj2001@gmail.com>
2020-11-25 10:26:15 +0100

    Added palindroom check

---
+++ 11-palindroom-check/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11_palindroom_check {
+  class Program {
+    static void Main(string[] args) {
+      // Ask the user for their string
+      Console.WriteLine("Uw string:");
+      string userInput = Console.ReadLine();
+
+      // Reverse the string
+      char[] userInputInChar = userInput.ToCharArray();
+      Array.Reverse(userInputInChar);
+      string userInputReversed = new string(userInputInChar);
+
+      // If it's the same reversed, it's paldindrome
+      if (userInput == userInputReversed) {
+        Console.WriteLine("De string is palindroom!");
+        return;
+      }
+
+      // If we've gotten to this point it's not palindrome
+      Console.WriteLine("De string is niet palindroom!");
+    }
+  }
+}
