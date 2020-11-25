84444fc2ddc6ea1129930a47dc67b78c5af62541
SlagterJ <slagterj2001@gmail.com>
2020-11-25 10:26:24 +0100

    Added priemgetallen

---
+++ 8-priemgetallen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _8_priemgetallen {
+  class Program {
+    static void Main(string[] args) {
+      for (int i = 0; i <= 100; i++) {
+        int counter = 0;
+
+        for (int j = 2; j <= i / 2; j++) {
+          if (i % j == 0) {
+            counter++;
+            break;
+          }
+        }
+
+        if (counter == 0 && i != 1) {
+          Console.WriteLine(i);
+        }
+      }
+    }
+  }
+}
