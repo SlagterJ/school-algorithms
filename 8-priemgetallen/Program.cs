3f8651504d3b2dd84c9ff803f6ed9a6fc7e07a40
SlagterJ <slagterj2001@gmail.com>
2020-11-25 10:48:13 +0100

    Added comments to priemgetallen

--- 8-priemgetallen/Program.cs
+++ 8-priemgetallen/Program.cs
@@ -3,9 +3,12 @@
 namespace _8_priemgetallen {
   class Program {
     static void Main(string[] args) {
+      // Loop 100 times
       for (int i = 0; i <= 100; i++) {
+        // Set a counter to use to detect prime numbers
         int counter = 0;
 
+        // Only let numbers through that have potential
         for (int j = 2; j <= i / 2; j++) {
           if (i % j == 0) {
             counter++;
@@ -13,6 +16,7 @@ namespace _8_priemgetallen {
           }
         }
 
+        // Print out prime numbers
         if (counter == 0 && i != 1) {
           Console.WriteLine(i);
         }
