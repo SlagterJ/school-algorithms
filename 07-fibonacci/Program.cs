469577075b2f5cc7eb757d0edb98eae4e65de484
SlagterJ <slagterj2001@gmail.com>
2020-12-02 13:06:43 +0100

    Renamed to better stuffs

---
+++ 07-fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _7_fibonacci {
+  class Program {
+    static void Main(string[] args) {
+      int a = 0;
+      int b = 1;
+
+      int i = 0;
+
+      while (i < 10) {
+        int temp = a;
+        a = b;
+        b = temp + b;
+
+        Console.WriteLine(b);
+        i++;
+      }
+    }
+  }
+}
--- 10-hanoi/Program.cs
+++ 10-hanoi/Program.cs
@@ -4,6 +4,7 @@ using _10_hanoi.GameFunctions;
 namespace _10_hanoi {
   class Program {
     static void Main(string[] args) {
+      Console.WriteLine("Welkom bij torens van Hanoi");
       Game game = new Game();
       game.Start();
     }
